7b597a36b27a06285c0deb28d8a4532cab30ab96
TraNguyenThanh <trant@haloasia.net>
2013-05-29 15:38:15 +0700

    This is version 0.0.2

---
+++ Decapic/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Decapic
+//
+//  Created by NTT on 5/29/13.
+//  Copyright (c) 2013 Halo Asia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
